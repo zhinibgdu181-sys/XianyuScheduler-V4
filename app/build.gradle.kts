6ae61ff1f40fc21422b687b82a8ff577bb5adbcf
zhinibgdu181-sys <zhinibgdu181@gmail.com>
2026-09-16 02:43:25 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         minSdk = 26
         targetSdk = 35
 
-        versionCode = 5
-        versionName = "4.1.0"
+        versionCode = 6
+        versionName = "4.2.0"
     }
 
     buildTypes {
