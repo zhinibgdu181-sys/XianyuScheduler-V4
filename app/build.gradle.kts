35f0ab22f9df389275913483fb1baa8264113e45
zhinibgdu181-sys <zhinibgdu181@gmail.com>
2026-09-18 23:00:19 +0800

    升级版本到4.43.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         minSdk = 26
         targetSdk = 35
 
-        versionCode = 55
-        versionName = "4.43.5"
+        versionCode = 56
+        versionName = "4.43.6"
     }
 
     // Stable signing support.
