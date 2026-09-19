3d8f19d7dd60f101a6fa6b3fcfd292702bf5db0b
zhinibgdu181-sys <zhinibgdu181@gmail.com>
2026-09-19 13:18:14 +0800

    test: cover minimal unlock plus shuffle popup OCR

--- app/src/test/java/com/zhinibgdu/xianyu/FruitGameRecoveryTest.java
+++ app/src/test/java/com/zhinibgdu/xianyu/FruitGameRecoveryTest.java
@@ -157,6 +157,8 @@ public class FruitGameRecoveryTest {
         assertTrue(FruitGameSolver.looksLikeBlockingFunctionPopupText(
                 "解锁所有檀位 [DJ使用 打乱 12%"));
         assertTrue(FruitGameSolver.looksLikeBlockingFunctionPopupText(
+                "赚闲鱼币 剩余 182 消除 第送 解锁 打乱 12%"));
+        assertTrue(FruitGameSolver.looksLikeBlockingFunctionPopupText(
                 "随机打乱 立即使用"));
         assertTrue(FruitGameSolver.looksLikeBlockingFunctionPopupText(
                 "解锁 解锁所有糟位 D使用 打乱 12%"));
