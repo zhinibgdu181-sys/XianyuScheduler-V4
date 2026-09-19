9c203b692e41867158854af9b8425ab2dd003c7f
zhinibgdu181-sys <zhinibgdu181@gmail.com>
2026-09-19 19:07:31 +0800

    V4.65: test human-experience admission gates

--- app/src/test/java/com/zhinibgdu/xianyu/FruitHumanExperienceStoreTest.java
+++ app/src/test/java/com/zhinibgdu/xianyu/FruitHumanExperienceStoreTest.java
@@ -38,4 +38,45 @@ public class FruitHumanExperienceStoreTest {
     public void noVerifiedProgressDoesNotBecomeExperience() {
         assertNull(FruitHumanExperienceStore.classify(200, 200, 2, 2, 40, 40, 30, 30));
     }
+
+    @Test
+    public void falseTransitionCannotReinforceStrategyMemory() {
+        assertEquals(false, FruitHumanExperienceStore.shouldReinforce(
+                new FruitHumanExperienceStore.Transition(
+                        FruitStrategyExperienceStore.STRATEGY_PAIR, false)));
+    }
+
+    @Test
+    public void unknownHumanActionCannotEnterStrategyMemory() {
+        assertEquals(false, FruitHumanExperienceStore.shouldReinforce(
+                new FruitHumanExperienceStore.Transition("MANUAL_TAP", true)));
+    }
+
+    @Test
+    public void verifiedKnownHumanStrategyCanReinforce() {
+        assertEquals(true, FruitHumanExperienceStore.shouldReinforce(
+                new FruitHumanExperienceStore.Transition(
+                        FruitStrategyExperienceStore.STRATEGY_PAIR, true)));
+    }
+
+    @Test
+    public void impossibleRemainingIncreaseIsRejected() {
+        FruitGameSolver.TeachingStateV464 a =
+                new FruitGameSolver.TeachingStateV464(200, 1, 40, 20, 10, 0, 1, 0);
+        FruitGameSolver.TeachingStateV464 b =
+                new FruitGameSolver.TeachingStateV464(202, 1, 40, 20, 10, 0, 1, 0);
+        assertEquals("remaining_increased", FruitHumanExperienceStore.rejectReason(a, b));
+    }
+
+    @Test
+    public void afterStateMustBeStableBeforeItIsTrusted() {
+        FruitGameSolver.TeachingStateV464 a =
+                new FruitGameSolver.TeachingStateV464(200, 1, 40, 20, 10, 0, 1, 0);
+        FruitGameSolver.TeachingStateV464 b =
+                new FruitGameSolver.TeachingStateV464(200, 2, 40, 19, 10, 0, 1, 0);
+        FruitGameSolver.TeachingStateV464 b2 =
+                new FruitGameSolver.TeachingStateV464(200, 2, 40, 19, 10, 0, 1, 0);
+        assertEquals(false, FruitHumanExperienceStore.sameStructuralState(a, b));
+        assertEquals(true, FruitHumanExperienceStore.sameStructuralState(b, b2));
+    }
 }
