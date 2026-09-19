3b32dc9123833d0a7100a4dedb5ce36164bce849
zhinibgdu181-sys <zhinibgdu181@gmail.com>
2026-09-19 17:20:31 +0100

    build: bump APK version to 4.44.10 (70)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         minSdk = 26
         targetSdk = 35
 
-        versionCode = 69
-        versionName = "4.44.9"
+        versionCode = 70
+        versionName = "4.44.10"
     }
 
     // Stable signing support.
