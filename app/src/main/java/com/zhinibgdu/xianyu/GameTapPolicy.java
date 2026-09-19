c7e0ff9242f12d7780170b496af8e80bfaf8b561
zhinibgdu181-sys <zhinibgdu181@gmail.com>
2026-09-19 13:51:38 +0800

    fix: allow repeated fruit popup close taps

--- app/src/main/java/com/zhinibgdu/xianyu/GameTapPolicy.java
+++ app/src/main/java/com/zhinibgdu/xianyu/GameTapPolicy.java
@@ -8,7 +8,8 @@ final class GameTapPolicy {
         double nx = x / (double) width, ny = y / (double) height;
         if ("水果游戏-开始游戏".equals(reason))
             return nx >= 560.0/1440 && nx <= 880.0/1440 && ny >= 2180.0/3120 && ny <= 2500.0/3120;
-        if ("水果游戏-关闭道具弹窗".equals(reason))
+        if ("水果游戏-关闭道具弹窗".equals(reason)
+                || (reason != null && reason.startsWith("水果游戏-继续关闭道具弹窗")))
             return nx >= 1160.0/1440 && nx <= 1325.0/1440 && ny >= 740.0/3120 && ny <= 960.0/3120;
         if (!"水果游戏-配对A".equals(reason)
                 && !"水果游戏-配对B".equals(reason)
